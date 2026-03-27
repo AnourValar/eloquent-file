0568ef92b8a1dd063ff42bf02f614fc2fd3f2b3d
Nikolay Novikov <AnourValar@gmail.com>
2026-03-27 19:40:12 +0300

    patch

--- src/app/Facades/EloquentFileFacade.php
+++ src/app/Facades/EloquentFileFacade.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Facade;
 class EloquentFileFacade extends Facade
 {
     /**
+     * Get the registered name of the component.
+     *
      * @return string
      */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return \AnourValar\EloquentFile\Services\FileService::class;
     }
